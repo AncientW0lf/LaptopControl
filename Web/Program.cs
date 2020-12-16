ce7288146d632f1561b19bb53a6c49e0470db8d9
LunarDiver <25Prozent@gmail.com>
2020-12-16 12:38:18 +0100

    Removed json in favor of setting url during init.

--- Web/Program.cs
+++ Web/Program.cs
@@ -21,6 +21,7 @@ namespace LaptopControlWeb
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:80");
                 });
     }
 }
